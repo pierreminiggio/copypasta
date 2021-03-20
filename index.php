cfb6a8c4537a12533f378c925a232edb81627950
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-03-20 21:17:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $link = 'https://www.miniggiodev.fr/league-of-legends-progresser-en-solo-q';
+    header('Location: ' . $link);
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El Famoso Copy Pasta</title>
+</head>
+<body>
+    <a href="<?php echo $link ?>">Cliquer pour être redirigé sur <?php echo $link ?></a>
+</body>
+</html>
